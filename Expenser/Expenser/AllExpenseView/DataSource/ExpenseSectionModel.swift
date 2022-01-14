a925964f8313127ddeaded81728e0d1efccbee74
Dushyant Singh <dushyantsingh1097@gmail.com>
2022-01-15 01:08:24 +0800

    Added Datasource for Expense model

---
+++ Expenser/Expenser/AllExpenseView/DataSource/ExpenseSectionModel.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseSectionModel.swift
+//  Expenser
+//
+//  Created by Dushyant Singh on 12/1/22.
+//
+
+import Foundation
+
+struct ExpenseSectionModel {
+    var header: String
+    var rows: [ExpenseObject]
+}
